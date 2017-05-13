376c53de531a2cd75db31e9285bbd6c3be0e7cd8
DorottyaKrisztics <dorka713@gmail.com>
2017-05-13 15:32:22 +0200

    database_connect function added: connection data come from variables

--- common_basicsql.py
+++ common_basicsql.py
@@ -1,4 +1,17 @@
 
+import psycopg2
+
+
+def database_connect(dbname_inp, user_inp, host_inp, password_inp):
+
+    connect_str = str("dbname='" + dbname_inp + "' user='" + user_inp + "' host='" + host_inp +
+                      "' password='" + password_inp + "'")
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    return cursor
+
+
 def get_inputs(list_labels, title):
     inputs = []
     print(title)
@@ -13,3 +26,4 @@ def print_menu(title, list_options, exit_message):
     for option in enumerate(list_options):
         print("({0}) {1}".format(option[0] + 1, option[1]))
     print("({0}) {1}".format("0", exit_message))
+
