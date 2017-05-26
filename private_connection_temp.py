d4287e88e1e8410616297630e97dd280247a98c7
DorottyaKrisztics <dorka713@gmail.com>
2017-05-26 14:41:35 +0200

    template to the private database connection

---
+++ private_connection_temp.py
@@ -0,0 +1,8 @@
+
+def private_connection():
+    private_data = {"dbname": "yourdbname",
+                    "user": "yourusername",
+                    "host": "localhost",
+                    "password": "yourpassword"}
+    return private_data
+
