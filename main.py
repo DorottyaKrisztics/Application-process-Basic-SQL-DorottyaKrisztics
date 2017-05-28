f164814f1085e6577ea8285a8256e8c2ee7056e1
DorottyaKrisztics <dorka713@gmail.com>
2017-05-28 17:30:38 +0200

    mentors page route defined

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import query_handler
+app = Flask(__name__)
+
+
+@app.route('/mentors', methods=['GET'])
+def mentors():
+
+    mentors_query = query_handler.mentors_and_schools()
+    print(mentors_query)
+    return render_template("mentors_school.html", mentors=mentors_query, title="Mentors")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
